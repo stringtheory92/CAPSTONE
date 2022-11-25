7d4220895a612d2233a28d5a8f3430ae69bcb7ba
Adam Kabak <adamkabak@yahoo.com>
2022-11-25 10:05:26 -0500

    Forum seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,24 @@ ap "Seeding..."
     User.create(user_name: Faker::Name.first_name, password: "123")
 end
 
+# ========== Forums ============================================================
 3.times do
-    MainForum.create(category: "[BG]", heading: Faker::Music.band)
+    main_forum = MainForum.create(category: "[BG]", heading: Faker::Music.genre)
+    4.times do
+        sub_forum = SubForum.create(heading: Faker::Music.band, main_forum_id: main_forum.id)
+        10.times do
+            ForumDiscussionTopic.create(heading: Faker::Music.album, sub_forum_id: sub_forum.id, user_id: rand(1..10))
+        end
+    end
 end
 3.times do
-    MainForum.create(category: "[DB]", heading: Faker::Music.band)
+    main_forum = MainForum.create(category: "[DB]", heading: Faker::Music.genre)
+    4.times do
+        sub_forum = SubForum.create(heading: Faker::Music.band, main_forum_id: main_forum.id)
+        10.times do
+            ForumDiscussionTopic.create(heading: Faker::Music.album, sub_forum_id: sub_forum.id, user_id: rand(1..10))
+        end
+    end
 end
 
 
