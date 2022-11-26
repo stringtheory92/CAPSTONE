3fa72f8e4977ab06766f9a705f991d01aa18392e
Adam Kabak <adamkabak@yahoo.com>
2022-11-26 16:28:22 -0500

    wrapped user and subforum with topic data for display

--- app/serializers/forum_discussion_topic_serializer.rb
+++ app/serializers/forum_discussion_topic_serializer.rb
@@ -1,7 +1,14 @@
 class ForumDiscussionTopicSerializer < ActiveModel::Serializer
-  attributes :id, :heading, :created_at, :updated_at
+  attributes :id, :heading, :created_at, :updated_at, :wrapped_sub_forum, :wrapped_user
 
   belongs_to :user
   belongs_to :sub_forum
+
+  def wrapped_sub_forum
+    self.object.sub_forum
+  end
+  def wrapped_user
+    self.object.user
+  end
   
 end
--- client/src/components/shared/MainForumItem.jsx
+++ client/src/components/shared/MainForumItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const MainForumItem = styled.li`
+  margin: 0.5rem;
   padding: 0.5rem 0.75rem;
   /* border: none; */
   font-weight: 600;
@@ -9,7 +10,9 @@ const MainForumItem = styled.li`
   color: var(--primary);
 
   background-color: var(--white);
-  border: 2px solid var(--dark-turquoise);
+  border-radius: 6px;
+  box-shadow: 2px 2px 3px #222;
+  /* border: 2px solid var(--dark-turquoise); */
   transition: background-color 0.2s;
 `;
 // const MainForumItem = styled.li`
