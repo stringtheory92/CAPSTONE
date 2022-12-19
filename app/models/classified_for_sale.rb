f5f11d58518e885a0f8079afb63a4e235a67c673
Adam Kabak <adamkabak@yahoo.com>
2022-12-19 15:20:16 -0500

    commit for safety

--- app/models/classified_for_sale.rb
+++ app/models/classified_for_sale.rb
@@ -4,12 +4,11 @@ class ClassifiedForSale < ApplicationRecord
     has_many :for_sale_messages
 
     has_one_attached :pic
-    # include Rails.application.routes.url_helpers
 
+# Not currently in use
     def for_sale_messages_method
         return self.for_sale_messages.map do |message|
-            # binding.pry
-        #  { **message.attributes, author: {**message.user.attributes}}
+       
           {**message.attributes, author: {**message.user.attributes, avatar: rails_blob_path(message.user.avatar, only_path: true)}}
         end
       end
--- app/serializers/classified_for_sale_serializer.rb
+++ app/serializers/classified_for_sale_serializer.rb
@@ -4,45 +4,29 @@ class ClassifiedForSaleSerializer < ActiveModel::Serializer
 
 
   def pic
-    
     rails_blob_url(object.pic, only_path: true) if object.pic.attached?
-   
   end
+
 
   def total_messages
     self.object.for_sale_messages.count
   end
 
-  def last_message
-    
+
+  def last_message  
     self.object.for_sale_messages.last
   end
 
+
   def for_sale_messages
-    
     return self.object.for_sale_messages.map do |message|
 
       message_hash = {**message.attributes, author: {**message.user.attributes}}
       message_hash[:author]['avatar'] = rails_blob_path(message.user.avatar.blob, only_path: true) if message.user.avatar.blob
       message_hash
-      # {**message.attributes, author: {**message.user.attributes, avatar: rails_blob_path(message.user.avatar.blob, only_path: true)}}
     end
   end
 
-
-  
-
-  # def last_message_by
-  #   name = User.find(object.for_sale_messages.last["user_id"]).user_name
-  #   return name
-
-      # object.for_sale_messages.last.user.user_name 
-    
-  # end
-
   belongs_to :classified_category
   belongs_to :user
-  # has_many :for_sale_messages
-
-
 end
