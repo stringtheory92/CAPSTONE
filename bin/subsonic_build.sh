33bfa02304bab89111b4213d57e41c0b7c8477b2
Adam Kabak <adamkabak@yahoo.com>
2022-12-22 12:56:48 -0500

    routes

--- bin/subsonic_build.sh
+++ bin/subsonic_build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Build commands for front end to create the production build
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean # These lines are commented out because we have an API only app
